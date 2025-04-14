3b6d24f6223b08ee67a3da48df8a7b4c158f83d2
miguel salviano <miguelcf.tg@gmail.com>
2025-04-14 11:36:17 -0300

    feat: CSS navbar added

--- index.php
+++ index.php
@@ -24,23 +24,27 @@
         <nav id="main-navbar" class="navbar navbar-expand-lg">
             <a href="<?= $BASE_URL ?>" class="navbar-brand">
                 <img src="<?= $BASE_URL ?>img/users/logo.svg" alt="Moviestar" id="logo">
-                <span id="moviestar-title">Moviestar</span>
+                <span id="moviestar-title">MovieStar</span>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
             <div>
-                <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                    <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar filmes" aria-label="Search">
-                    <button class="btn my-2 my-sm-0" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </form>
+                <div class="mb-3">
+                    <form action="" method="GET" id="search-form" class="my-2 my-lg-2">
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-md" placeholder="Buscar Filmes" aria-label="Buscar Filmes" name="q">
+                            <button class="btn btn-default" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar | Cadastrar</a>
                     </li>
                 </ul>
             </div>
