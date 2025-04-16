4db377039b6696eba4e0e47af4fd756c8f248348
miguel salviano <miguelcf.tg@gmail.com>
2025-04-16 15:50:48 -0300

    feat: home page

--- index.php
+++ index.php
@@ -1,13 +1,50 @@
 <?php
-require_once('templates/header.php')
+require_once("templates/header.php");
+
+require_once("dao/MovieDAO.php");
+
+// DAO dos filmes
+$movieDAO = new MovieDAO($conn, $BASE_URL);
+
+$latestMovies = $movieDAO->getLatestMovies();
+
+$actionMovies = $movieDAO->getMoviesByCategory("Ação");
+
+$comedyMovies = $movieDAO->getMoviesByCategory("Comédia");
+
 ?>
-
-<main>
-    <div id="main-container" class="container-fluid">
-        <h1>se ta maluco</h1>
+<div id="main-container" class="container-fluid">
+    <h2 class="section-title">Filmes novos</h2>
+    <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
+    <div class="movies-container">
+        <?php foreach ($latestMovies as $movie): ?>
+            <?php require("templates/movie-card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($latestMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+        <?php endif; ?>
     </div>
-</main>
-
+    <h2 class="section-title">Ação</h2>
+    <p class="section-description">Veja os melhores filmes de ação</p>
+    <div class="movies-container">
+        <?php foreach ($actionMovies as $movie): ?>
+            <?php require("templates/movie-card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($actionMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de ação cadastrados!</p>
+        <?php endif; ?>
+    </div>
+    <h2 class="section-title">Comédia</h2>
+    <p class="section-description">Veja os melhores filmes de comédia</p>
+    <div class="movies-container">
+        <?php foreach ($comedyMovies as $movie): ?>
+            <?php require("templates/movie-card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($comedyMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de comédia cadastrados!</p>
+        <?php endif; ?>
+    </div>
+</div>
 <?php
-require_once('templates/footer.php')
+require_once("templates/footer.php");
 ?>
